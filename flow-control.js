48ee93a1e60a22ba05c3389ecdc3b9dd928e15f3
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2017-10-24 08:40:49 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,11 @@
 function basicTeenager(age) {
-if(age >= 13 && age <= 16) {
+if(age >= 13 && age <= 19) {
   return 'You are a teenager!';
 }
 }
 
 function teenager(age) {
-  if(age >= 13 && age <= 16) {
+  if(age >= 13 && age <= 19) {
     return 'You are a teenager!';
   } else {
     return 'You are not a teenager';
@@ -14,7 +14,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+if(age <= 13 && age >= 19) {
+  return 'You are a teenager!';
+} else if (age <= 12) {
+  return 'You are a kid'
+} else {
+  return 'You are a grownup'
+}
 }
 
 function ternaryTeenager(age) {
